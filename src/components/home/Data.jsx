887cf9eff4b47f20b52f36eaabd3ecd2ee26cf74
Mohamed Ait Fares <medaitfares2001@gmail.com>
2023-10-24 17:30:07 +0100

    annuler

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -4,7 +4,7 @@ export const Data = () => {
   return (
     <div className="home__data">
       <h1 className="home__title">
-        Mohamed
+        Mohameddddd
         <br /> Ait Fares
         <svg
           width="36"
